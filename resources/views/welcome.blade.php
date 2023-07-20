3bf086630ede50535dccc7527b20bdc9fc26ff54
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-07-20 16:48:16 +0200

    test to catch titles from array-comics.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Welcome')
 
 @section('main-section')
+    @dd($title)
     <h1>
         welcome
     </h1>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $title = config('comics.title');
+    return view('welcome', compact('title'));
+})->name('homepage');
